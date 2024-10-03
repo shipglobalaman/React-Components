c3d373f68562513a0f3b88555ab2f3c9d94aa268
Aman <aman.tripathi@shipglobal.in>
2024-10-03 12:29:10 +0530

    Component Added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import InputSpace from './components/InputSpace'
+
+function App() {
+
+  return (
+    <>
+     <center className="font-bold">I'm Creating Components</center>
+     <InputSpace></InputSpace>
+    </>
+  )
+}
+
+export default App
