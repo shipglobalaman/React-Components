84cba92a5c94c0d94434e30966d3315f6381d3f0
Aman <aman.tripathi@shipglobal.in>
2024-10-03 17:56:51 +0530

    Used Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import InputSpace from './components/InputSpace'
 
 function App() {
 
   return (
-    <>
-     <center className="font-bold">I'm Creating Components</center>
-     <InputSpace></InputSpace>
+    < >
+    <div className="bg-amber-50">
+     <center className="font-bold text-3xl">I'm Creating Components</center>
+     <InputSpace type="number" placeholder="00.00" text="Number"></InputSpace>
+     <InputSpace type="text" placeholder="Enter Text"text="Text Box"></InputSpace>
+     <InputSpace type="email" placeholder="example@gmail.com"text="Email Box"></InputSpace>
+     <InputSpace type="checkbox" text="Check Box" ></InputSpace>
+     <InputSpace type="radio"text="Radio Box"></InputSpace>
+     <InputSpace type="date" text="Date Box"></InputSpace>
+     <InputSpace type="date and time" placeholder="Enter Text"></InputSpace>
+     <InputSpace type="time" text="Time Box"></InputSpace>
+     <InputSpace type="file" text="File Box"></InputSpace>
+     <InputSpace type="password" placeholder="Enter Password" text="Security Box"></InputSpace>
+     <InputSpace type="search" placeholder="Search here" text="Search Box"></InputSpace>
+     <InputSpace type="tel" placeholder="123-45-678" text="Telephone Box"></InputSpace>
+     <InputSpace type="url" placeholder="Enter Text"text="URL Box"></InputSpace>
+     <InputSpace type="submit" ></InputSpace>
+     </div>
     </>
+    
   )
 }
 
--- src/components/InputSpace.jsx
+++ src/components/InputSpace.jsx
@@ -1,39 +1,19 @@
-function InputSpace(){
-    return <>
-    <div className="flex justify-center items-center ">
-  <div>
-    <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">
-      Price
-    </label>
-    <div className="relative mt-2 rounded-md shadow-sm">
-      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-        <span className="text-gray-500 sm:text-sm">$</span>
-      </div>
-      <input
-        id="price"
-        name="price"
-        type="text"
-        placeholder="0.00"
-        className="block w-96 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-      />
-      <div className="absolute inset-y-0 right-0 flex items-center">
-        <label htmlFor="currency" className="sr-only">
-          Currency
+function InputSpace(props) {
+    const {type,placeholder,text}=props;
+  return (
+    <>
+      <div className="flex justify-center items-center ">
+        <label htmlFor=""> {text}
+        <input
+          id="price"
+          name="price"
+          type={type}
+          placeholder={placeholder}
+          className="block w-96 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 my-3"
+        />
         </label>
-        <select
-          id="currency"
-          name="currency"
-          className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-        >
-          <option>USD</option>
-          <option>CAD</option>
-          <option>EUR</option>
-        </select>
       </div>
-    </div>
-  </div>
-</div>
-
     </>
+  );
 }
-export default InputSpace
+export default InputSpace;
